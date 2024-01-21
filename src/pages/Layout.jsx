ec8f02677a103cc8c019b05a7aa64338dfe24c3f
Elher Encarnacion <encarnacionelher@gmail.com>
2024-01-21 22:01:27 +0800

    Update Layout.jsx

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -107,7 +107,7 @@ const Layout = () => {
       </nav>
       
       {/* Button NavBar (Mobile) */}
-      <button onClick={() => setIsOpen(!isOpen)} className="md:hidden flex-1 flex justify-end">
+      <button onClick={() => setIsOpen(!isOpen)} className="md:hidden flex-1 flex justify-end" aria-label="Toggle menu">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
         </svg>
