b9a43bcf3ae942c64f4394d2e83635936cb238a8
Elher Encarnacion <encarnacionelher@gmail.com>
2024-02-05 11:24:43 +0800

    lostpetlayoutr

--- src/pages/LosttPet.jsx
+++ src/pages/LosttPet.jsx
@@ -66,17 +66,60 @@ const LosttPet = () => {
 
         <div className='flex-1 p-0 md:p-7'>
 
-          {/* Reward */}
+
+          <div className='flex pb-7'>
+            <div className='w-1/2 space-y-3'>
+            
+             {/* Location */}
+             {pet.placeLost && (    
+              <p className='text-sm flex items-center gap-2 font-semibold'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 41 40" fill="none">
+                <path d="M37.1389 28.1279V33.0717C37.1407 33.5307 37.0467 33.985 36.8629 34.4055C36.679 34.826 36.4093 35.2035 36.0711 35.5138C35.7329 35.824 35.3337 36.0602 34.8989 36.2073C34.4641 36.3543 34.0035 36.4089 33.5464 36.3676C28.4753 35.8166 23.6043 34.0838 19.3245 31.3084C15.3428 28.7783 11.967 25.4024 9.43684 21.4207C6.65177 17.1215 4.91856 12.2268 4.37763 7.13297C4.33645 6.67726 4.39061 6.21797 4.53666 5.78433C4.68271 5.3507 4.91745 4.95222 5.22594 4.61428C5.53443 4.27634 5.90991 4.00633 6.32846 3.82145C6.74702 3.63658 7.19949 3.54087 7.65706 3.54044H12.6009C13.4007 3.53257 14.176 3.81578 14.7824 4.33728C15.3888 4.85878 15.7849 5.58299 15.8968 6.37492C16.1055 7.95706 16.4925 9.51052 17.0504 11.0057C17.2721 11.5955 17.3201 12.2365 17.1886 12.8528C17.0572 13.4691 16.7519 14.0348 16.3088 14.4828L14.2159 16.5757C16.5618 20.7014 19.9779 24.1175 24.1036 26.4634L26.1965 24.3705C26.6446 23.9275 27.2102 23.6221 27.8265 23.4907C28.4428 23.3593 29.0838 23.4072 29.6737 23.629C31.1688 24.1869 32.7223 24.5739 34.3044 24.7825C35.1049 24.8955 35.836 25.2987 36.3586 25.9155C36.8813 26.5323 37.1589 27.3197 37.1389 28.1279Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              </svg>
+                {pet.placeLost}
+              </p>
+               )}
+
+                 {/* Date Lost */}
+              {pet.dateLost && (    
+              <p className='text-sm flex items-center gap-2 font-semibold'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 41 41" fill="none">
+                <path d="M32.1953 7.21387H9.12403C7.30375 7.21387 5.82812 8.68949 5.82812 10.5098V33.5811C5.82812 35.4013 7.30375 36.877 9.12403 36.877H32.1953C34.0156 36.877 35.4912 35.4013 35.4912 33.5811V10.5098C35.4912 8.68949 34.0156 7.21387 32.1953 7.21387Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M27.251 3.91797V10.5098" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M14.0674 3.91797V10.5098" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M5.82812 17.1016H35.4912" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              </svg>
+                {pet.dateLost}
+              </p>
+               )}
+
+                {/* Location */}
+            {pet.contact && (    
+              <p className='text-sm flex items-center gap-2 font-semibold'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 41 40" fill="none">
+                <path d="M37.1389 28.1279V33.0717C37.1407 33.5307 37.0467 33.985 36.8629 34.4055C36.679 34.826 36.4093 35.2035 36.0711 35.5138C35.7329 35.824 35.3337 36.0602 34.8989 36.2073C34.4641 36.3543 34.0035 36.4089 33.5464 36.3676C28.4753 35.8166 23.6043 34.0838 19.3245 31.3084C15.3428 28.7783 11.967 25.4024 9.43684 21.4207C6.65177 17.1215 4.91856 12.2268 4.37763 7.13297C4.33645 6.67726 4.39061 6.21797 4.53666 5.78433C4.68271 5.3507 4.91745 4.95222 5.22594 4.61428C5.53443 4.27634 5.90991 4.00633 6.32846 3.82145C6.74702 3.63658 7.19949 3.54087 7.65706 3.54044H12.6009C13.4007 3.53257 14.176 3.81578 14.7824 4.33728C15.3888 4.85878 15.7849 5.58299 15.8968 6.37492C16.1055 7.95706 16.4925 9.51052 17.0504 11.0057C17.2721 11.5955 17.3201 12.2365 17.1886 12.8528C17.0572 13.4691 16.7519 14.0348 16.3088 14.4828L14.2159 16.5757C16.5618 20.7014 19.9779 24.1175 24.1036 26.4634L26.1965 24.3705C26.6446 23.9275 27.2102 23.6221 27.8265 23.4907C28.4428 23.3593 29.0838 23.4072 29.6737 23.629C31.1688 24.1869 32.7223 24.5739 34.3044 24.7825C35.1049 24.8955 35.836 25.2987 36.3586 25.9155C36.8813 26.5323 37.1589 27.3197 37.1389 28.1279Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              </svg>
+                {pet.contact}
+              </p>
+               )}
+
+
+               
+                         {/* Reward */}
           <div className='flex justify-between pb-7'>
             <div className='flex justify-between space-x-3'>
               <button className="bg-red-600 text-white px-3 font-bold py-1 rounded-md">Reward: ₱ {pet.reward}</button>
             </div>
           </div>
 
-          <div className='flex pb-7'>
+           
+
+          
+
+            </div>
+
             <div className='w-1/2 space-y-3'>
-              
-            {/* Animal Type */}
+               {/* Animal Type */}
             {pet.animalType && (
               <p className='text-sm flex items-center gap-2 font-semibold'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 28 26" fill="none">
@@ -120,33 +163,9 @@ const LosttPet = () => {
                 {pet.size}
               </p>
             )}
+           
 
-            {/* Date Lost */}
-              {pet.dateLost && (    
-              <p className='text-sm flex items-center gap-2 font-semibold'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 41 41" fill="none">
-                <path d="M32.1953 7.21387H9.12403C7.30375 7.21387 5.82812 8.68949 5.82812 10.5098V33.5811C5.82812 35.4013 7.30375 36.877 9.12403 36.877H32.1953C34.0156 36.877 35.4912 35.4013 35.4912 33.5811V10.5098C35.4912 8.68949 34.0156 7.21387 32.1953 7.21387Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                <path d="M27.251 3.91797V10.5098" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                <path d="M14.0674 3.91797V10.5098" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                <path d="M5.82812 17.1016H35.4912" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-              </svg>
-                {pet.dateLost}
-              </p>
-               )}
 
-            </div>
-
-            <div className='w-1/2 space-y-3'>
-
-            {/* Location */}
-            {pet.contact && (    
-              <p className='text-sm flex items-center gap-2 font-semibold'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 41 40" fill="none">
-                <path d="M37.1389 28.1279V33.0717C37.1407 33.5307 37.0467 33.985 36.8629 34.4055C36.679 34.826 36.4093 35.2035 36.0711 35.5138C35.7329 35.824 35.3337 36.0602 34.8989 36.2073C34.4641 36.3543 34.0035 36.4089 33.5464 36.3676C28.4753 35.8166 23.6043 34.0838 19.3245 31.3084C15.3428 28.7783 11.967 25.4024 9.43684 21.4207C6.65177 17.1215 4.91856 12.2268 4.37763 7.13297C4.33645 6.67726 4.39061 6.21797 4.53666 5.78433C4.68271 5.3507 4.91745 4.95222 5.22594 4.61428C5.53443 4.27634 5.90991 4.00633 6.32846 3.82145C6.74702 3.63658 7.19949 3.54087 7.65706 3.54044H12.6009C13.4007 3.53257 14.176 3.81578 14.7824 4.33728C15.3888 4.85878 15.7849 5.58299 15.8968 6.37492C16.1055 7.95706 16.4925 9.51052 17.0504 11.0057C17.2721 11.5955 17.3201 12.2365 17.1886 12.8528C17.0572 13.4691 16.7519 14.0348 16.3088 14.4828L14.2159 16.5757C16.5618 20.7014 19.9779 24.1175 24.1036 26.4634L26.1965 24.3705C26.6446 23.9275 27.2102 23.6221 27.8265 23.4907C28.4428 23.3593 29.0838 23.4072 29.6737 23.629C31.1688 24.1869 32.7223 24.5739 34.3044 24.7825C35.1049 24.8955 35.836 25.2987 36.3586 25.9155C36.8813 26.5323 37.1589 27.3197 37.1389 28.1279Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-              </svg>
-                {pet.contact}
-              </p>
-               )}
             </div>
           </div>
         </div>
