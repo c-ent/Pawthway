52b8a181e23361fee03bf6893da12a9881062174
Elher Encarnacion <encarnacionelher@gmail.com>
2024-02-06 11:19:42 +0800

    comments

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   
   <React.StrictMode>
     <BrowserRouter>
-      <SessionProvider> {/* Wrap App with SessionProvider */}
+      <SessionProvider> 
           <App />
       </SessionProvider>
     </BrowserRouter>
