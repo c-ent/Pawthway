39a5f149e3d7292b72bd3267049c0a5a0ab39d4d
Elher Encarnacion <encarnacionelher@gmail.com>
2024-02-26 11:32:15 +0800

    foundpets home

--- src/components/FoundPetCard.jsx
+++ src/components/FoundPetCard.jsx
@@ -1,12 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import dogplaceholder from '../../images/images/dogplaceholder.png';
+import { formatDistance, parseISO } from 'date-fns';
+
 const FoundPetCard = ({ pet }) => {
   const [isLoading, setIsLoading] = useState(true);
 
+
+  function formatDate(dateString) {
+    try {
+      const date = parseISO(dateString);
+      const now = new Date();
+      return formatDistance(date, now, { addSuffix: true });
+    } catch (error) {
+      console.error('Error formatting date:', error);
+      return dateString; // Return the original string if there's an error
+    }
+  }
+
+  
   return (
     <div className="rounded-md  bg-[#F5F5F5] space-y-2 text-sm group  hover:bg-[#C1C1C1]">
       <div className="relative">
-        <img src={pet.imageURL  ? pet.imageURL : dogplaceholder } alt={pet.description} className="w-full h-40 md:h-64 object-cover" />
+        <img src={pet.imageURL  ? pet.imageURL : dogplaceholder } alt={pet.description} className="w-full h-40 md:h-80 object-cover rounded-md" />
+        <button className="absolute top-[8px] right-[1%] bg-[#d9d9d962] text-black px-3 py-1 font-semibold rounded-md">
+         {formatDate(pet.found_date)}
+        </button>
       </div>
       
       {/* <h2 className="text-2xl font-bold">{pet.description}</h2> */}
@@ -15,7 +33,7 @@ const FoundPetCard = ({ pet }) => {
             <div className="">
            
                 <div className='flex  space-x-2'>
-                  <p>Where:</p>
+                  <p>Place:</p>
                   <p className='font-bold'>{pet.found_location}</p>
                 </div>
 
